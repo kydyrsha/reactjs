58e24536f3e90a267b4c01a45feb6b66a526b1c2
kydyrsha <sanatkydyrsha@gmail.com>
2021-10-07 16:13:18 +0600

    create app | jsx

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+const text = 'Hello World!'
+
+const elem = (
+    <div>
+        <h2>{text}</h2>
+        <input type="text"/>
+        <button>Click</button>
+    </div>
+)
+
+ReactDOM.render(
+    elem,
+  document.getElementById('root')
+);
+
